96a63563e6184fe6ef43a34600eef1fc7e11c85c
ebyau <bebyau@gmail.com>
2022-11-17 11:19:29 +0300

    fixed conflicting branch

--- accounts/models.py
+++ accounts/models.py
@@ -3,17 +3,32 @@ from django.db import models
 from django.utils import timezone
 from django.utils.timezone import localtime
 
+from django.contrib.auth.models import User
+
+from cloudinary.models import CloudinaryField
+from cloudinary_storage.storage import VideoMediaCloudinaryStorage
+from cloudinary_storage.validators import validate_video
+
 # Create your models here.
 
-class Customer(models.Model):
-    name = models.CharField(max_length=50)
+#
+# class Customer(models.Model):
+#     name = models.CharField(max_length=200, null=True)
+#     email = models.CharField(max_length=200, null=True)
+#     date_created = models.DateTimeField(auto_now_add=True)
+#
+#     def __str__(self) -> str:
+#         return self.name
+
+class Audio(models.Model):
+
+    name = models.CharField(max_length=200,null=True)
     message = models.TextField()
-    file = models.FileField(upload_to='audios/',null=True)
-    date_created = models.DateTimeField(default=timezone.now(),blank=False)
+    date_created = models.DateTimeField(default=timezone.now(), blank=False)
+    # file = models.ImageField(upload_to='kin-keepers/', blank=True, null=True, storage=VideoMediaCloudinaryStorage(),
+                              # validators=[validate_video])
+    file = CloudinaryField(resource_type='video',null=True,folder='kin-keepers/')
 
 
     def __str__(self) -> str:
-        return self.name + ' ' + localtime(self.date_created).strftime("%Y-%m-%d %H:%M")
-
-
-    
+        return str(self.file)
